46362b64599fe9d22be461f8dad5bf37bd1ee1b0
Jonathan <jonathan.frohne@web.de>
2019-06-12 10:27:25 +0200

    Test

--- JRobots-Competition/src/EmptyBot.java
+++ JRobots-Competition/src/EmptyBot.java
@@ -1,5 +1,8 @@
 import jrobots.utils.*;
+import jrobots.*;
 import jrobots.simulation.simulationObjects.JRobot2015_2;
+import jrobots.simulation.simulationObjects.Pilot;
+import jrobots.simulation.simulationObjects.launchables.Bullet;
 
 /** A minimal functional Bot, ready for being programmed. 
  * <p>Please enter the documentation via the method <tt>actions()</tt>.
@@ -12,7 +15,11 @@ public class EmptyBot extends JRobot2015_2 {
 	@Override
 	protected void actions() {
 		// TODO implement brain
-		
+		new Pilot();
+		while(true) {
+			Angle angle1 = Angle.EAST;
+			new Scan(Angle.EAST, Angle.NORTH, 2.0, new Vector(3, 3), 3.0);
+		}
 	}
 
 }
