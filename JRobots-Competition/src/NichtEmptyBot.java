84a79c123cd06cf34ab10e769840dc5a412a7a81
Jonathan <jonathan.frohne@web.de>
2019-06-14 10:33:48 +0200

    Added Scan

---
+++ JRobots-Competition/src/NichtEmptyBot.java
@@ -0,0 +1,67 @@
+import jrobots.utils.*;
+
+import java.awt.Color;
+
+import jrobots.*;
+import jrobots.simulation.simulationObjects.JRobot2011;
+import jrobots.simulation.simulationObjects.Pilot;
+import jrobots.simulation.simulationObjects.launchables.Bullet;
+
+/** A minimal functional Bot, ready for being programmed. 
+ * <p>Please enter the documentation via the method <tt>actions()</tt>.
+ * <p>Please rename uniquely. The individual name will be displayed in the GUI.
+ * @see JRobot2015_2#actions()
+ */
+public class NichtEmptyBot extends JRobot2011 {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void init() {
+		setNameColor(Color.BLUE);
+		setBodyColor(Color.LIGHT_GRAY);
+		setTurretColor(Color.RED);
+	}
+	
+	
+	protected void actions() {
+		// TODO implement brain
+		scan();
+		setAutopilot(Angle.NORTH, getMaxForwardVelocity());
+		if(getEnergy() >= getEnergyConsumptionProjectile()) {
+		}
+		//scan();
+		
+	}
+	
+	protected Scan scan(){
+		setScanAperture(getMaxScanAperture());
+		setScanDirection(Angle.NORTH);
+		if(getLastScan().isTargetLocated()) {
+			return getLastScan();
+		}
+		else {
+			setScanDirection(Angle.EAST);
+			if(getLastScan().isTargetLocated()) {
+				return getLastScan();
+			}
+			else {
+				setScanDirection(Angle.SOUTH);
+				if(getLastScan().isTargetLocated()) {
+					return getLastScan();
+				}
+				else {
+					setScanDirection(Angle.WEST);
+					if(getLastScan().isTargetLocated()) {
+						return getLastScan();
+					}
+				}
+			}
+		}
+		return null;
+	}
+	
+	public Angle scanDirection() {
+		return new Angle(172, "Degrees");
+	}
+
+}
